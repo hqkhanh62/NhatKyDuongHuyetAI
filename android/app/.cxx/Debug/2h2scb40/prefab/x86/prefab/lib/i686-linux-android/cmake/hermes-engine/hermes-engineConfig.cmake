e9b8190afb0c3180f3432b599a56d73688758b50
HQKhanh <hqkhanh62@gmail.com>
2026-07-12 20:23:02 +0700

    Fix exit code 127 V3.0.0

--- android/app/.cxx/Debug/2h2scb40/prefab/x86/prefab/lib/i686-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Khanh/.gradle/caches/9.3.1/transforms/67daa95c2663ca072cb96062d5bba35d/workspace/transformed/hermes-android-250829098.0.14-debug/prefab/modules/hermesvm/libs/android.x86/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Khanh/.gradle/caches/9.3.1/transforms/67daa95c2663ca072cb96062d5bba35d/workspace/transformed/hermes-android-250829098.0.14-debug/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- android/app/.cxx/Debug/4a1r1o2u/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/fbjni/fbjniConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET fbjni::fbjni)
-add_library(fbjni::fbjni SHARED IMPORTED)
-set_target_properties(fbjni::fbjni PROPERTIES
-    IMPORTED_LOCATION "C:/Users/Khanh/.gradle/caches/9.3.1/transforms/3ad149e23399ba01b2dd5822e3fced4b/workspace/transformed/fbjni-0.7.0/prefab/modules/fbjni/libs/android.arm64-v8a/libfbjni.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/Khanh/.gradle/caches/9.3.1/transforms/3ad149e23399ba01b2dd5822e3fced4b/workspace/transformed/fbjni-0.7.0/prefab/modules/fbjni/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
