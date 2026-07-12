ae6ea6f5fb4a97007d047d81230f57843d9d6ff1
HQKhanh <hqkhanh62@gmail.com>
2026-07-12 22:22:56 +0700

    Android Compose#6

--- android/app/src/main/java/com/nhatkyduonghuyetai/MainActivity.kt
+++ android/app/src/main/java/com/nhatkyduonghuyetai/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.nhatkyduonghuyetai
 
-import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 
-class MainActivity : ReactActivity() {
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "NhatKyDuongHuyetAI"
-
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+        setContent {
+            MaterialTheme {
+                Surface {
+                    // TODO: thay bằng màn hình chính nhật ký đường huyết của bạn
+                    Text(text = "Nhat Ky Duong Huyet AI")
+                }
+            }
+        }
+    }
 }
